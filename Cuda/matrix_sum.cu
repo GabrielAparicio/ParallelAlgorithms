a216c19695e091bdaec061ab4485414403946c4c
GabrielAparicio <sgaparicio22@gmail.com>
2017-06-05 10:46:28 -0500

    Update matrix_sum.cu

--- Cuda/matrix_sum.cu
+++ Cuda/matrix_sum.cu
@@ -41,7 +41,7 @@ __global__ void matrix_sum3(int A[], int B[], int C[], int fil, int col)
    {
       for(i=0;i<fil;i++)
       {
-         C[index*i] = A[index*i] + B[index*i];
+         C[col*i + i] = A[col*i+i] + B[col*i+i];
       }
    }
    
